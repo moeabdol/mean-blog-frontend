aec309a053f806b394bf69f50c0cb858c7891c8b
moeabdol <mohd.a.saed@gmail.com>
2017-09-04 09:33:54 +0300

    Implement basic home and dashboard pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { HomeComponent } from "./components/home/home.component";
+import {
+  DashboardComponent
+} from "./components/dashboard/dashboard.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "**", component: HomeComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
