73085052e980494e108cff45e789274b388d9311
moeabdol <mohd.a.saed@gmail.com>
2017-09-06 14:21:17 +0300

    Implement blog.component basic html

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
 
 @Component({
   selector: "blog",
@@ -6,12 +12,57 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./blog.component.css"]
 })
 export class BlogComponent implements OnInit {
+  public form: FormGroup;
   public processingNewPost: boolean = false;
+  public submittingNewPost: boolean = false;
   public loadingBlogPosts: boolean = false;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this._formBuilder.group({
+      title: ["", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50),
+        this.validateAlphaNumeric
+      ])],
+      body: ["", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(500),
+      ])]
+    });
+  }
+
+  validateAlphaNumeric(control: FormControl): { [s: string]: boolean } {
+    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/);
+    if (regExp.test(control.value)) {
+      return null;
+    } else {
+      return { "invalidAlphaNumeric": true };
+    }
+  }
+
+  enableNewBlogForm() {
+    this.form.controls["title"].enable();
+    this.form.controls["body"].enable();
+  }
+
+  disableNewBlogForm() {
+    this.form.controls["title"].disable();
+    this.form.controls["body"].disable();
+  }
+
+  onBlogSubmit() {
+    console.log(this.form.value);
+    this.submittingNewPost = true;
+    this.disableNewBlogForm();
+
+    const blog = {
+      title: this.form.get("title").value,
+      body: this.form.get("body").value
+    };
   }
 
   newBlogForm() {
@@ -27,5 +78,10 @@ export class BlogComponent implements OnInit {
   }
 
   draftComment() {
+  }
+
+  goBack() {
+    this.processingNewPost = false;
+    this.form.reset();
   }
 }
